f81c617798d488d26fc71387373de3405ac8a970
Todd Miller <tmiller@fairwaytech.com>
2016-09-12 14:22:19 -0700

    Added OpenExam request POJO and validation

---
+++ client/src/main/java/tds/exam/OpenExam.java
@@ -0,0 +1,97 @@
+package tds.exam;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+/**
+ * Information required to open an exam
+ */
+public class OpenExam {
+    @NotNull
+    private String clientName;
+
+    @NotNull
+    @Min(1)
+    private long studentId;
+
+    @NotNull
+    private String assessmentKey;
+
+    @NotNull
+    @Min(1)
+    private int maxOpportunities;
+
+    @NotNull
+    private UUID sessionId;
+
+    /**
+     * @return unique client name
+     */
+    public String getClientName() {
+        return clientName;
+    }
+
+    /**
+     * @param clientName unique client name
+     */
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    /**
+     * @return identifier for the student taking the exam
+     */
+    public long getStudentId() {
+        return studentId;
+    }
+
+    /**
+     * @param studentId identifier for the student taking the exam
+     */
+    public void setStudentId(long studentId) {
+        this.studentId = studentId;
+    }
+
+    /**
+     * @return identifier for the assessment
+     */
+    public String getAssessmentKey() {
+        return assessmentKey;
+    }
+
+    /**
+     * @param assessmentKey identifier for the assessment
+     */
+    public void setAssessmentKey(String assessmentKey) {
+        this.assessmentKey = assessmentKey;
+    }
+
+    /**
+     * @return max number of opportunities the student has to take the exam
+     */
+    public int getMaxOpportunities() {
+        return maxOpportunities;
+    }
+
+    /**
+     * @param maxOpportunities max number of opportunities the student has to take the exam
+     */
+    public void setMaxOpportunities(int maxOpportunities) {
+        this.maxOpportunities = maxOpportunities;
+    }
+
+    /**
+     * @return unique identifier for the session
+     */
+    public UUID getSessionId() {
+        return sessionId;
+    }
+
+    /**
+     * @param sessionId unique identifier for the session
+     */
+    public void setSessionId(UUID sessionId) {
+        this.sessionId = sessionId;
+    }
+}
