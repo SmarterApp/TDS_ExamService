643fea29e7ca2bb4da8a5d92107f851ab0494bd8
Todd Miller <tmiller@fairwaytech.com>
2016-09-20 14:10:30 -0700

    Add date completed SQL script

---
+++ service/src/main/resources/db/migration/V1474314885__exam_add_date_completed_column.sql
@@ -0,0 +1,9 @@
+/***********************************************************************************************************************
+  File: V1474314885__exam_add_date_completed_column.sql
+
+  Desc: Adds a completed date column to the exam tble
+
+***********************************************************************************************************************/
+USE exam;
+
+ALTER TABLE exam ADD COLUMN date_completed DATETIME(3) DEFAULT NULL;
