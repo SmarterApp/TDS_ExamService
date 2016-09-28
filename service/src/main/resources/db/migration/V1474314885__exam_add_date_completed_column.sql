4c3f0af51275eb52a6d6a178112070d93f5bac7e
Todd Miller <tmiller@fairwaytech.com>
2016-09-28 08:00:54 -0700

    fix mispelling

--- service/src/main/resources/db/migration/V1474314885__exam_add_date_completed_column.sql
+++ service/src/main/resources/db/migration/V1474314885__exam_add_date_completed_column.sql
@@ -1,7 +1,7 @@
 /***********************************************************************************************************************
   File: V1474314885__exam_add_date_completed_column.sql
 
-  Desc: Adds a completed date column to the exam tble
+  Desc: Adds a completed date column to the exam table
 
 ***********************************************************************************************************************/
 USE exam;
