43ed5e281df808800aeaac5c76177c66442b941b
Todd Miller <tmiller@fairwaytech.com>
2016-11-01 08:44:47 -0700

    Pull out configurations into their own @Config classes

--- service/src/main/java/tds/exam/configuration/data/DataSourceConfiguration.java
+++ service/src/main/java/tds/exam/configuration/data/DataSourceConfiguration.java
@@ -1,10 +1,13 @@
 package tds.exam.configuration.data;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -37,5 +40,12 @@ public class DataSourceConfiguration {
     @Bean(name = "queryJdbcTemplate")
     public NamedParameterJdbcTemplate queryJdbcTemplate() {
         return new NamedParameterJdbcTemplate(queriesDataSource());
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(@Qualifier("commandDataSource") DataSource dataSource) {
+        final DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource);
+        return dataSourceTransactionManager;
     }
 }
