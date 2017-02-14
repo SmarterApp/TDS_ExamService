12eda27ef21a45ac8ad523dd47ff15e02627d2e4
Jeff Johnson <jeffjohnson9046@hotmail.com>
2017-02-14 11:56:14 -0800

    TDS-672: update javadoc

--- service/src/main/java/tds/exam/services/ExamineeService.java
+++ service/src/main/java/tds/exam/services/ExamineeService.java
@@ -1,7 +1,5 @@
 package tds.exam.services;
 
-import java.util.UUID;
-
 import tds.exam.Exam;
 import tds.exam.ExamineeContext;
 
@@ -13,8 +11,7 @@ public interface ExamineeService {
      * Insert the {@link tds.exam.ExamineeAttribute}s and {@link tds.exam.ExamineeRelationship}s for this
      * {@link tds.exam.Exam} for the specified {@link tds.exam.ExamineeContext}.
      *
-     * @param examId The unique identifier of the {@link tds.exam.Exam} for which attributes and relationships are being
-     *               stored
+     * @param exam    The {@link tds.exam.Exam} for which attributes and relationships are being stored
      * @param context The {@link tds.exam.ExamineeContext} that describes the state of the examniee's information when
      *                these attributes and relationships were collected
      */
