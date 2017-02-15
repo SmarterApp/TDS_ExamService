cd09ea3dd12f228b763f959a80a227a5cbb070f3
wtritch <will@gearlaunch.com>
2017-02-14 18:09:55 -0800

    Use the external configuration service for configurations.  Add a local-development configuration profile with sample property values.

--- service/src/main/docker/docker-startup.sh
+++ service/src/main/docker/docker-startup.sh
@@ -6,30 +6,4 @@
 #
 #-----------------------------------------------------------------------------------------------------------------------
 
-java \
-    -Dspring.ds_queries.jdbcUrl="jdbc:mysql://${EXAM_DB_HOST}/${EXAM_DB_NAME}" \
-    -Dspring.ds_queries.username="${EXAM_DB_USER}" \
-    -Dspring.ds_queries.password="${EXAM_DB_PASSWORD}" \
-    -Dspring.ds_queries.driver-class-name=com.mysql.jdbc.Driver \
-    -Dspring.ds_commands.jdbcUrl="jdbc:mysql://${EXAM_DB_HOST}/${EXAM_DB_NAME}" \
-    -Dspring.ds_commands.username="${EXAM_DB_USER}" \
-    -Dspring.ds_commands.password="${EXAM_DB_PASSWORD}" \
-    -Dspring.ds_commands.driver-class-name=com.mysql.jdbc.Driver \
-    -Dspring.datasource.testWhileIdle=true \
-    -Dspring.datasource.timeBetweenEvictionRunsMillis=60000 \
-    -Dspring.datasource.validationQuery="SELECT 1" \
-    -Dexam-service.session-url=http://session:8080/ \
-    -Dexam-service.student-url=http://student:8080/ \
-    -Dexam-service.assessment-url=http://assessment:8080/ \
-    -Dexam-service.config-url=http://config:8080/ \
-    -Dflyway.enabled=${EXAM_FLYWAY_ENABLED} \
-    -Dflyway.url="jdbc:mysql://${EXAM_DB_HOST}/${EXAM_DB_NAME}" \
-    -Dflyway.user="${EXAM_DB_USER}" \
-    -Dflyway.password="${EXAM_DB_PASSWORD}" \
-    -jar /tds-exam-service.jar \
-    --server-port="8080" \
-    --server.undertow.buffer-size=16384 \
-    --server.undertow.buffers-per-region=20 \
-    --server.undertow.io-threads=64 \
-    --server.undertow.worker-threads=512 \
-    --server.undertow.direct-buffers=true
+java -jar /tds-exam-service.jar
