ebc3dcc08a50536270c61788c609833bf6703d5c
Todd Miller <tmiller@fairwaytech.com>
2017-02-19 13:46:31 -0800

    Clean imports

--- service/src/main/java/tds/exam/configuration/data/DataSourceConfiguration.java
+++ service/src/main/java/tds/exam/configuration/data/DataSourceConfiguration.java
@@ -1,7 +1,5 @@
 package tds.exam.configuration.data;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -10,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 /**
  * Configuration for DataSources.  Queries
