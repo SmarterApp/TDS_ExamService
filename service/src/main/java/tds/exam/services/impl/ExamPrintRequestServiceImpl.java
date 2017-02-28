eb81031af987f976ec909c837144b5bb88b9d1e7
Ernie Munoz <emunoz@fairwaytech.com>
2017-02-27 18:27:17 -0800

    Adding @Transactional to insert print request

--- service/src/main/java/tds/exam/services/impl/ExamPrintRequestServiceImpl.java
+++ service/src/main/java/tds/exam/services/impl/ExamPrintRequestServiceImpl.java
@@ -2,6 +2,7 @@ package tds.exam.services.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
 import java.util.UUID;
@@ -24,6 +25,7 @@ public class ExamPrintRequestServiceImpl implements ExamPrintRequestService{
     }
 
     @Override
+    @Transactional
     public void insert(final ExamPrintRequest examPrintRequest) {
         examPrintRequestCommandRepository.insert(examPrintRequest);
     }
