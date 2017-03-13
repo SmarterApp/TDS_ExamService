a744ce82186a296c69ef7a16d624ada3178f280f
Jeff Treuting <jtreuting@fairwaytech.com>
2017-03-13 16:49:56 -0700

    Remove leftover imports

--- service/src/main/java/tds/exam/utils/listeners/OnCompletedStatusExamChangeListener.java
+++ service/src/main/java/tds/exam/utils/listeners/OnCompletedStatusExamChangeListener.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import tds.common.Response;
