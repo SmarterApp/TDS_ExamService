43f9f217bd6183cdd3799785a0667094e42dce63
Todd Miller <tmiller@fairwaytech.com>
2017-03-17 14:55:10 -0700

    temp

--- service/src/main/java/tds/exam/services/scoring/ItemServiceImpl.java
+++ service/src/main/java/tds/exam/services/scoring/ItemServiceImpl.java
@@ -1,4 +1,18 @@
 package tds.exam.services.scoring;
 
-public class ItemServiceImpl  {
+import java.util.Optional;
+
+import tds.score.model.Item;
+import tds.score.services.ItemService;
+
+public class ItemServiceImpl implements ItemService {
+    @Override
+    public Optional<Item> findItemByStimulusKey(final long bankKey, final long stimulusKey) {
+        return null;
+    }
+
+    @Override
+    public Optional<Item> findItemByKey(final long bankKey, final long itemKey) {
+        return null;
+    }
 }
