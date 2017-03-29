d1e1da14f6be4e386f362c428f3ccc6114c04d3a
Todd Miller <tmiller@fairwaytech.com>
2017-03-29 13:57:30 -0700

    Added documentation

--- scoring/src/main/java/tds/score/services/ResponseService.java
+++ scoring/src/main/java/tds/score/services/ResponseService.java
@@ -16,7 +16,7 @@ public interface ResponseService {
     /**
      * Updates responses with the score
      *
-     * @param examInstance    {@link tds.score.model.ExamInstance} that contains the information for the exam
+     * @param examInstance   {@link tds.score.model.ExamInstance} that contains the information for the exam
      * @param responseUpdate {@link tds.student.sql.data.IItemResponseUpdate} that contains the response information
      * @param score          score for the item
      * @param scoreStatus    the score status
@@ -24,9 +24,21 @@ public interface ResponseService {
      * @param scoreLatency   the latency
      * @param pageDuration   the page duration
      * @return a {@link TDS.Shared.Data.ReturnStatus} that contains whether the update was successful
-     * @throws ReturnStatusException
+     * @throws ReturnStatusException if there is any type of error or validation condition failure
      */
     ReturnStatus updateScoredResponse(ExamInstance examInstance, IItemResponseUpdate responseUpdate, int score, String scoreStatus, String scoreRationale, long scoreLatency, long pageDuration) throws ReturnStatusException;
 
-    ReturnStatus updateItemScore (UUID examId, IItemResponseScorable responseScorable, int score, String scoreStatus, String scoreRationale, String scoreDimensions) throws ReturnStatusException;
+    /**
+     * Updates a specific item with an updated score.
+     *
+     * @param examId           the exam id
+     * @param responseScorable the {@link tds.student.sql.data.IItemResponseScorable} containing some score inforamtion
+     * @param score            score for the item
+     * @param scoreStatus      the score status for the item
+     * @param scoreRationale   the score rationale
+     * @param scoreDimensions  the dimensions associated with the score
+     * @return
+     * @throws ReturnStatusException if there is an unexpected error
+     */
+    ReturnStatus updateItemScore(UUID examId, IItemResponseScorable responseScorable, int score, String scoreStatus, String scoreRationale, String scoreDimensions) throws ReturnStatusException;
 }
