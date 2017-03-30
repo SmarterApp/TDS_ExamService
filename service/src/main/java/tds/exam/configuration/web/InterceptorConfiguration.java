36f5b4bd3c06c316dc473503476766a6c22497d8
Jeffrey C Jones <jjones@fairwaytech.com>
2017-03-30 01:10:19 -0700

    Add EventLoggerInterceptor to log incoming calls.

--- service/src/main/java/tds/exam/configuration/web/InterceptorConfiguration.java
+++ service/src/main/java/tds/exam/configuration/web/InterceptorConfiguration.java
@@ -1,11 +1,14 @@
 package tds.exam.configuration.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import tds.exam.services.ExamApprovalService;
+import tds.exam.web.interceptors.EventLoggerInterceptor;
 import tds.exam.web.interceptors.VerifyAccessInterceptor;
 
 /**
@@ -13,7 +16,11 @@ import tds.exam.web.interceptors.VerifyAccessInterceptor;
  */
 @Configuration
 public class InterceptorConfiguration extends WebMvcConfigurerAdapter {
+
     private final ExamApprovalService examApprovalService;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Autowired
     public InterceptorConfiguration(ExamApprovalService examApprovalService) {
@@ -22,6 +29,7 @@ public class InterceptorConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new VerifyAccessInterceptor(examApprovalService)).addPathPatterns("/exam/**");;
+        registry.addInterceptor(new EventLoggerInterceptor(new ObjectMapper())).addPathPatterns("/exam/**");
+        registry.addInterceptor(new VerifyAccessInterceptor(examApprovalService)).addPathPatterns("/exam/**");
     }
 }
