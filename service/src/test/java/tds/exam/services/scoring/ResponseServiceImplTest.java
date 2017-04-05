56fe90ca5b2fe4af9750310aa8d1d3240899e62a
Todd Miller <tmiller@fairwaytech.com>
2017-04-05 12:53:00 -0700

    Add unit tests to response service.

--- service/src/test/java/tds/exam/services/scoring/ResponseServiceImplTest.java
+++ service/src/test/java/tds/exam/services/scoring/ResponseServiceImplTest.java
@@ -1,5 +1,7 @@
 package tds.exam.services.scoring;
 
+import TDS.Shared.Exceptions.ReturnStatusException;
+import org.joda.time.Instant;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,8 +9,13 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import tds.exam.Exam;
+import tds.exam.ExamScoringStatus;
+import tds.exam.ExamStatusCode;
+import tds.exam.builder.ExamBuilder;
 import tds.exam.repositories.ExamItemCommandRepository;
 import tds.exam.repositories.ExamItemQueryRepository;
 import tds.exam.services.ConfigService;
@@ -16,6 +23,11 @@ import tds.exam.services.ExamPageService;
 import tds.exam.services.ExamService;
 import tds.score.model.ExamInstance;
 import tds.score.services.ResponseService;
+import tds.student.sql.data.IItemResponseUpdate;
+import tds.student.sql.data.ItemResponseUpdate;
+
+import static io.github.benas.randombeans.api.EnhancedRandom.random;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ResponseServiceImplTest {
@@ -49,12 +61,23 @@ public class ResponseServiceImplTest {
     public void tearDown() {
     }
 
-    @Test
-    public void itShouldThrowWhenExamCannotBeFound() {
+    @Test (expected = ReturnStatusException.class)
+    public void itShouldThrowWhenExamCannotBeFound() throws ReturnStatusException {
         UUID examId = UUID.randomUUID();
         ExamInstance examInstance = ExamInstance.create(examId, UUID.randomUUID(), UUID.randomUUID(), "SBAC_PT");
-//        IItemResponseUpdate update = new ItemResponseUpdate("testKey", "testId", "language", )
-//        responseService.updateScoredResponse(examInstance, )
+        IItemResponseUpdate update = random(ItemResponseUpdate.class);
+        when(mockExamService.findExam(examId)).thenReturn(Optional.empty());
+        responseService.updateScoredResponse(examInstance, update, 1, ExamScoringStatus.SCORED.toString(), "rationale", 1, 200);
     }
 
+    @Test (expected = ReturnStatusException.class)
+    public void itShouldThrowIfExamNotInCorrectStatus() throws ReturnStatusException {
+        Exam exam = new ExamBuilder()
+            .withStatus(new ExamStatusCode(ExamStatusCode.STATUS_PAUSED), Instant.now())
+            .build();
+        ExamInstance examInstance = ExamInstance.create(exam.getId(), UUID.randomUUID(), UUID.randomUUID(), "SBAC_PT");
+        IItemResponseUpdate update = random(ItemResponseUpdate.class);
+        when(mockExamService.findExam(exam.getId())).thenReturn(Optional.of(exam));
+        responseService.updateScoredResponse(examInstance, update, 1, ExamScoringStatus.SCORED.toString(), "rationale", 1, 200);
+    }
 }
