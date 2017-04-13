797169e0d3821d3d733c470bf1f8e03cdfc1c0df
Ernie Munoz <emunoz@fairwaytech.com>
2017-04-13 13:38:07 -0700

    Bumping pom version changing POST to PUT

--- service/src/main/java/tds/exam/web/endpoints/ExamItemController.java
+++ service/src/main/java/tds/exam/web/endpoints/ExamItemController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,7 +54,7 @@ public class ExamItemController {
         return ResponseEntity.ok(nextPage);
     }
 
-    @PostMapping("/{examId}/item/{position}/review")
+    @PutMapping("/{examId}/item/{position}/review")
     @VerifyAccess
     ResponseEntity<NoContentResponseResource> markItemForReview(@PathVariable final UUID examId,
                                                                 @PathVariable final int position,
