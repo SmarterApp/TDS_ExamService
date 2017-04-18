041193e97533fb6d1ca1841159c694eeec5bdbb0
Todd Miller <tmiller@fairwaytech.com>
2017-04-18 13:28:55 -0700

    Cleaned up the settings for item scoring

--- scoring/src/main/java/tds/score/configuration/ItemScoreSettings.java
+++ scoring/src/main/java/tds/score/configuration/ItemScoreSettings.java
@@ -16,117 +16,79 @@ public class ItemScoreSettings {
         return enabled;
     }
 
+    public void setEnabled(final boolean enabled) {
+        this.enabled = enabled;
+    }
+
     public boolean isDebug() {
         return debug;
+    }
+
+    public void setDebug(final boolean debug) {
+        this.debug = debug;
     }
 
     public String getServerUrl() {
         return serverUrl;
     }
 
+    public void setServerUrl(final String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
     public String getCallbackUrl() {
         return callbackUrl;
+    }
+
+    public void setCallbackUrl(final String callbackUrl) {
+        this.callbackUrl = callbackUrl;
     }
 
     public boolean isTimerEnabled() {
         return timerEnabled;
     }
 
+    public void setTimerEnabled(final boolean timerEnabled) {
+        this.timerEnabled = timerEnabled;
+    }
+
     public boolean isAlwaysLoadRubric() {
         return alwaysLoadRubric;
+    }
+
+    public void setAlwaysLoadRubric(final boolean alwaysLoadRubric) {
+        this.alwaysLoadRubric = alwaysLoadRubric;
     }
 
     public int getTimerInterval() {
         return timerInterval;
     }
 
+    public void setTimerInterval(final int timerInterval) {
+        this.timerInterval = timerInterval;
+    }
+
     public int getTimerPendingMinutes() {
         return timerPendingMinutes;
+    }
+
+    public void setTimerPendingMinutes(final int timerPendingMinutes) {
+        this.timerPendingMinutes = timerPendingMinutes;
     }
 
     public int getTimerMaxAttempts() {
         return timerMaxAttempts;
     }
 
+    public void setTimerMaxAttempts(final int timerMaxAttempts) {
+        this.timerMaxAttempts = timerMaxAttempts;
+    }
+
     public boolean isEncryptionEnabled() {
         return encryptionEnabled;
     }
 
-    public static final class ItemScoreSettingsBuilder {
-        private boolean enabled = true;
-        private boolean debug = false;
-        private String serverUrl;
-        private String callbackUrl;
-        private boolean timerEnabled = true;
-        private boolean alwaysLoadRubric = false;
-        private int timerInterval = 5;
-        private int timerPendingMinutes = 15;
-        private int timerMaxAttempts = 10;
-        private boolean encryptionEnabled = false;
-
-        public ItemScoreSettingsBuilder withEnabled(boolean enabled) {
-            this.enabled = enabled;
-            return this;
-        }
-
-        public ItemScoreSettingsBuilder withDebug(boolean debug) {
-            this.debug = debug;
-            return this;
-        }
-
-        public ItemScoreSettingsBuilder withServerUrl(String serverUrl) {
-            this.serverUrl = serverUrl;
-            return this;
-        }
-
-        public ItemScoreSettingsBuilder withCallbackUrl(String callbackUrl) {
-            this.callbackUrl = callbackUrl;
-            return this;
-        }
-
-        public ItemScoreSettingsBuilder withTimerEnabled(boolean timerEnabled) {
-            this.timerEnabled = timerEnabled;
-            return this;
-        }
-
-        public ItemScoreSettingsBuilder withAlwaysLoadRubric(boolean alwaysLoadRubric) {
-            this.alwaysLoadRubric = alwaysLoadRubric;
-            return this;
-        }
-
-        public ItemScoreSettingsBuilder withTimerInterval(int timerInterval) {
-            this.timerInterval = timerInterval;
-            return this;
-        }
-
-        public ItemScoreSettingsBuilder withTimerPendingMinutes(int timerPendingMinutes) {
-            this.timerPendingMinutes = timerPendingMinutes;
-            return this;
-        }
-
-        public ItemScoreSettingsBuilder withTimerMaxAttempts(int timerMaxAttempts) {
-            this.timerMaxAttempts = timerMaxAttempts;
-            return this;
-        }
-
-        public ItemScoreSettingsBuilder withEncryptionEnabled(boolean encryptionEnabled) {
-            this.encryptionEnabled = encryptionEnabled;
-            return this;
-        }
-
-        public ItemScoreSettings build() {
-            ItemScoreSettings itemScoreSettings = new ItemScoreSettings();
-            itemScoreSettings.callbackUrl = this.callbackUrl;
-            itemScoreSettings.timerEnabled = this.timerEnabled;
-            itemScoreSettings.timerInterval = this.timerInterval;
-            itemScoreSettings.serverUrl = this.serverUrl;
-            itemScoreSettings.alwaysLoadRubric = this.alwaysLoadRubric;
-            itemScoreSettings.enabled = this.enabled;
-            itemScoreSettings.timerPendingMinutes = this.timerPendingMinutes;
-            itemScoreSettings.timerMaxAttempts = this.timerMaxAttempts;
-            itemScoreSettings.debug = this.debug;
-            itemScoreSettings.encryptionEnabled = this.encryptionEnabled;
-            return itemScoreSettings;
-        }
+    public void setEncryptionEnabled(final boolean encryptionEnabled) {
+        this.encryptionEnabled = encryptionEnabled;
     }
 }
