e6996919620b871e3bd7d6d323b81419e5239dbe
Todd Miller <tmiller@fairwaytech.com>
2017-04-18 21:21:35 -0700

    Added the item selectio nconfiguration information

--- service/src/main/java/tds/exam/configuration/web/ExamServiceApplicationConfiguration.java
+++ service/src/main/java/tds/exam/configuration/web/ExamServiceApplicationConfiguration.java
@@ -3,11 +3,13 @@ package tds.exam.configuration.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
 import tds.common.configuration.CacheConfiguration;
 import tds.common.configuration.RedisClusterConfiguration;
 import tds.common.configuration.RestTemplateConfiguration;
 import tds.common.configuration.SecurityConfiguration;
 import tds.common.web.advice.ExceptionAdvice;
+import tds.exam.configuration.item.selection.ItemSelectionConfiguration;
 
 /**
  * Configuration for Exam microservice.
@@ -18,7 +20,8 @@ import tds.common.web.advice.ExceptionAdvice;
     RestTemplateConfiguration.class,
     RedisClusterConfiguration.class,
     CacheConfiguration.class,
-    SecurityConfiguration.class
+    SecurityConfiguration.class,
+    ItemSelectionConfiguration.class
 })
 public class ExamServiceApplicationConfiguration {
 }
