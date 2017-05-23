abccf187ccee4022b508a9691acea868feba87ea
Jeff Johnson <jeffjohnson9046@hotmail.com>
2017-05-23 10:14:12 -0700

    TDS-1001: exam segment query optimization

--- service/src/main/resources/db/migration/V1495496675__exam_exam_segment_query_tuning.sql
+++ service/src/main/resources/db/migration/V1495496675__exam_exam_segment_query_tuning.sql
@@ -6,3 +6,9 @@
 ***********************************************************************************************************************/
 USE exam;
 
+  DROP INDEX fk_exam_segment_event_pk_exam_segment ON exam.exam_segment_event;
+  DROP INDEX ix_created_at ON exam.exam_segment_event;
+
+  ALTER TABLE exam.exam_segment_event
+	ADD CONSTRAINT fk_exam_segment_event_exam_segment_id_segment_position
+    FOREIGN KEY (exam_id, segment_position) REFERENCES exam_segment(exam_id, segment_position);
