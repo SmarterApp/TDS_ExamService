0c466d3f06dde447921b93f41112351226b180b2
Jeff Johnson <jeffjohnson9046@hotmail.com>
2017-05-23 13:02:09 -0700

    TDS-1001: correct schema modification issue

--- service/src/main/resources/db/migration/V1495496675__exam_exam_segment_query_tuning.sql
+++ service/src/main/resources/db/migration/V1495496675__exam_exam_segment_query_tuning.sql
@@ -5,10 +5,11 @@
 
 ***********************************************************************************************************************/
 USE exam;
+ALTER TABLE exam_segment_event
+  DROP FOREIGN KEY fk_exam_segment_event_pk_exam_segment;
 
-  DROP INDEX fk_exam_segment_event_pk_exam_segment ON exam.exam_segment_event;
-  DROP INDEX ix_created_at ON exam.exam_segment_event;
+  DROP INDEX ix_created_at ON exam_segment_event;
 
-  ALTER TABLE exam.exam_segment_event
+  ALTER TABLE exam_segment_event
 	ADD CONSTRAINT fk_exam_segment_event_exam_segment_id_segment_position
     FOREIGN KEY (exam_id, segment_position) REFERENCES exam_segment(exam_id, segment_position);
