8300e9f56d459bd42b7ad81628684cef20af4ef9
Jeff Johnson <jeffjohnson9046@hotmail.com>
2017-05-25 10:55:54 -0700

    TDS-1003: preivous item group performance tuning

--- service/src/main/resources/db/migration/V1495584287__exam_history_query_tuning.sql
+++ service/src/main/resources/db/migration/V1495584287__exam_history_query_tuning.sql
@@ -6,8 +6,22 @@
 ***********************************************************************************************************************/
 USE exam;
 
-ALTER TABLE exam.history
-	MODIFY id CHAR(36);
+ALTER TABLE exam.exam_event
+	DROP FOREIGN KEY fk_exam_event_exam_id;
 
-ALTER TABLE exam.history
-	MODIFY exam_id CHAR(36) NOT NULL;
+ALTER TABLE exam.exam_event
+	ADD student_id BIGINT(20) NOT NULL AFTER exam_id;
+
+ALTER TABLE exam.exam_event
+	ADD assessment_id VARCHAR(255) NOT NULL AFTER student_id;
+
+UPDATE
+	exam.exam_event AS ee
+JOIN
+	exam.exam AS e
+    ON e.id = ee.exam_id
+SET
+	ee.student_id = e.student_id,
+    ee.assessment_id = e.assessment_id;
+
+CREATE INDEX ix_exam_id_student_id_assessment_id ON exam.exam_event(exam_id, student_id, assessment_id);
