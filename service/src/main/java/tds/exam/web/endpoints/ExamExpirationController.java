45b99723243c62224bef328c176165ee0c0289d6
Todd Miller <tmiller@fairwaytech.com>
2017-11-17 14:53:59 -0800

    Added the path variable for client name

--- service/src/main/java/tds/exam/web/endpoints/ExamExpirationController.java
+++ service/src/main/java/tds/exam/web/endpoints/ExamExpirationController.java
@@ -3,6 +3,7 @@ package tds.exam.web.endpoints;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,7 @@ public class ExamExpirationController {
     }
 
     @PutMapping(value = "expire/{clientName}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<ExpiredExamInformation>> expireExams(final String clientName) {
+    ResponseEntity<Collection<ExpiredExamInformation>> expireExams(@PathVariable  final String clientName) {
         return ResponseEntity.ok(examExpirationService.expireExams(clientName));
     }
 }
