af1a4a12067867193fe1a8bffade07fa74449e81
Todd Miller <tmiller@fairwaytech.com>
2017-11-17 14:23:44 -0800

    Add endpoint to expire exams

---
+++ service/src/main/java/tds/exam/web/endpoints/ExamExpirationController.java
@@ -0,0 +1,29 @@
+package tds.exam.web.endpoints;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+import tds.exam.ExpiredExamInformation;
+import tds.exam.services.ExamExpirationService;
+
+@RestController
+@RequestMapping("/exam")
+public class ExamExpirationController {
+    private final ExamExpirationService examExpirationService;
+
+    @Autowired
+    public ExamExpirationController(final ExamExpirationService examExpirationService) {
+        this.examExpirationService = examExpirationService;
+    }
+
+    @PutMapping(value = "expire/{clientName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Collection<ExpiredExamInformation>> expireExams(final String clientName) {
+        return ResponseEntity.ok(examExpirationService.expireExams(clientName));
+    }
+}
---
+++ service/src/test/java/tds/exam/web/endpoints/ExamExpirationControllerTest.java
@@ -0,0 +1,42 @@
+package tds.exam.web.endpoints;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
+
+import tds.exam.ExpiredExamInformation;
+import tds.exam.services.ExamExpirationService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ExamExpirationControllerTest {
+    @Mock
+    private ExamExpirationService mockExamExpirationService;
+
+    private ExamExpirationController examExpirationController;
+
+    @Before
+    public void setUp() {
+        examExpirationController = new ExamExpirationController(mockExamExpirationService);
+    }
+
+    @Test
+    public void shouldExpireExams() {
+        ExpiredExamInformation info = new ExpiredExamInformation(1L, "key", "id", UUID.randomUUID());
+        when(mockExamExpirationService.expireExams("SBAC")).thenReturn(Collections.singletonList(info));
+        ResponseEntity<Collection<ExpiredExamInformation>> response = examExpirationController.expireExams("SBAC");
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).containsExactly(info);
+    }
+}
