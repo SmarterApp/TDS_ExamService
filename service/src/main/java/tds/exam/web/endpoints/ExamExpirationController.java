fd5d2d7e681632de72b830b778af9b4938639a82
Todd Miller <tmiller@fairwaytech.com>
2017-11-20 08:43:40 -0800

    Use a post rather than put for exam expiration

--- service/src/main/java/tds/exam/web/endpoints/ExamExpirationController.java
+++ service/src/main/java/tds/exam/web/endpoints/ExamExpirationController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +23,7 @@ public class ExamExpirationController {
         this.examExpirationService = examExpirationService;
     }
 
-    @PutMapping(value = "expire/{clientName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "expire/{clientName}", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Collection<ExpiredExamInformation>> expireExams(@PathVariable  final String clientName) {
         return ResponseEntity.ok(examExpirationService.expireExams(clientName));
     }
