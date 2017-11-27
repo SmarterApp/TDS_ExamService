711ebd66c2a4e14fe28d0e65fefcc36dfcd4da84
Todd Miller <tmiller@fairwaytech.com>
2017-11-27 13:00:41 -0800

    Fix formatting

--- service/src/test/java/tds/exam/services/impl/ExamExpirationServiceImplTest.java
+++ service/src/test/java/tds/exam/services/impl/ExamExpirationServiceImplTest.java
@@ -15,9 +15,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import tds.assessment.Assessment;
