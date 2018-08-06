ac74df61ab670ceafd5b772cffcc7546a8e71193
Jeffrey C Jones <jjones@fairwaytech.com>
2018-08-06 00:01:22 -0700

    Disable mongodb autoconfiguration - no mongo needed.

--- service/src/main/java/tds/exam/ExamServiceApplication.java
+++ service/src/main/java/tds/exam/ExamServiceApplication.java
@@ -16,10 +16,12 @@ package tds.exam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class ExamServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExamServiceApplication.class, args);
